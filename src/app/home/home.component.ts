8deaf30353b1dfaf38b81d924817e27630146556
JesusTafolla <chuy_tafolla@hotmail.com>
2021-10-21 11:11:33 -0500

    Fixes to all problems

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
